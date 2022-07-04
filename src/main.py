e02662405fab1df7743681fdd7f2da56f8cb62cb
Richard Wells <rwells1703@gmail.com>
2022-07-04 13:50:16 +0100

    Refactored light control functions into a class.

--- src/main.py
+++ src/main.py
@@ -1,59 +1,72 @@
 from chip import FabricAdmin
 from chip import ChipStack
 
-# Initial setup
-def setup_device_controller():
-    controllerNodeId = 1
+class Controller:
+    def __init__(self, controller_node_id, device_ip, device_setup_pin_code, device_node_id):
+        # Define comissionable device's network info (IP and Matter)
+        self.device_ip = device_ip
+        self.device_setup_pin_code = device_setup_pin_code
+        self.device_node_id = device_node_id
 
-    chipStack = ChipStack.ChipStack(persistentStoragePath='/tmp/chip-device-ctrl-storage.json', installDefaultLogHandler=False, bluetoothAdapter=None)
-    fabricAdmin = FabricAdmin.FabricAdmin()
-    devCtrl = fabricAdmin.NewController(nodeId=controllerNodeId, useTestCommissioner=True)
+        self.controller_node_id = controller_node_id
 
-    return devCtrl
+        self.startup()
 
-# Connect to the device and commission it onto the fabric via the existing IP network
-def commission_device(devCtrl, node_id):
-    # Define comissionable device's network info (IP and Matter)
-    ip = "fd11:1111:1122:0:d81e:e2ff:f8db:b323".encode("utf-8")
-    setup_pin_code = 20202021
+    # Initial setup for the device controller
+    def startup(self):
+        self.chipStack = ChipStack.ChipStack(persistentStoragePath='/tmp/chip-device-ctrl-storage.json', installDefaultLogHandler=False, bluetoothAdapter=None)
+        self.fabricAdmin = FabricAdmin.FabricAdmin()
+        self.devCtrl = self.fabricAdmin.NewController(nodeId=self.controller_node_id, useTestCommissioner=True)
 
-    # Commission the device over the existing IP network
-    devCtrl.CommissionIP(ip, setup_pin_code, node_id)
+    # Connect to the device and commission it onto the fabric via the existing IP network
+    def commission_device(self):
+        # Commission the device over the existing IP network
+        self.devCtrl.CommissionIP(self.device_ip.encode("utf-8"), self.device_setup_pin_code, self.device_node_id)
 
+    # Switch the light on/off
+    def toggle_light(self):
+        cluster = "OnOff"
+        command = "Toggle"
+        endpoint = 1
+        group_id = 1
+        args = {}
 
-# Switch the light on/off
-def toggle_light(devCtrl, node_id, endpoint, group_id):
-    cluster = "OnOff"
-    command = "Toggle"
-    args = {}
+        response = self.devCtrl.ZCLSend(cluster, command, self.device_node_id, endpoint, group_id, args)
+        return response
 
-    response = devCtrl.ZCLSend(cluster, command, node_id, endpoint, group_id, args)
-    return response
+    # Read the on/off value of the light
+    def read_light(self):
+        cluster = "OnOff"
+        attribute = "OnOff"
+        endpoint = 1
+        group_id = 1
 
-# Read the on/off value of the light
-def read_light(devCtrl, node_id, endpoint, group_id):
-    cluster = "OnOff"
-    attribute = "OnOff"
+        result = self.devCtrl.ZCLReadAttribute(cluster, attribute, self.device_node_id, endpoint, group_id)
+        return result
 
-    result = devCtrl.ZCLReadAttribute(cluster, attribute, node_id, endpoint, group_id)
-    return result
+    # Shutdown device controller
+    def shutdown(self):
+        self.devCtrl.Shutdown()
+
 
 # Example will connect to a light, then toggle it on and off reading its value
 def main():
-    devCtrl = setup_device_controller()
+    controller = Controller(1, "2a00:23a8:89b:da01:5bac:df45:7887:b359", 20202021, 1234)
 
-    node_id = 1234
-    endpoint = 1
-    group_id = 1
+    while True:
+        choice = input(">>>")
 
-    commission_device(devCtrl, node_id)
-
-    print(toggle_light(devCtrl, node_id, endpoint, group_id))
-    print(read_light(devCtrl, node_id, endpoint, group_id))
-    print(toggle_light(devCtrl, node_id, endpoint, group_id))
-    print(read_light(devCtrl, node_id, endpoint, group_id))
-
-    devCtrl.Shutdown()
+        if choice == "commission":
+            controller.commission_device()
+        elif choice == "toggle":
+            print(controller.toggle_light())
+        elif choice == "read":
+            print(controller.read_light())
+        elif choice == "exit" or choice == "quit" or choice == "q":
+            controller.shutdown()
+            exit()
+        else:
+            print("Unsupported choice")
 
 if __name__ == "__main__":
     main()
