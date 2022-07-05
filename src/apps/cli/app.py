c42c59f09698456d803b4894d5d807d7e874e05f
Richard Wells <rwells1703@gmail.com>
2022-07-05 15:44:11 +0100

    Added CLI app command for getting port/address.

--- src/apps/cli/app.py
+++ src/apps/cli/app.py
@@ -3,7 +3,7 @@ from ...controller import Controller
 # Example will connect to a light, then toggle it on and off reading its value
 def main():
     controller = Controller(1, "2a00:23a8:89b:da01:5bac:df45:7887:b359", 20202021, 1234)
-
+    
     while True:
         choice = input(">")
 
@@ -13,6 +13,8 @@ def main():
             print(controller.toggle_light())
         elif choice == "read":
             print(controller.read_light())
+        elif choice == "address":
+            print(controller.devCtrl.GetAddressAndPort(1234))
         elif choice == "exit" or choice == "quit" or choice == "q":
             controller.shutdown()
             exit()
