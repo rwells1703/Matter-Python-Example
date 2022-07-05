4a212b895ec7a5d407a589389630700ffd340a96
Richard Wells <rwells1703@gmail.com>
2022-07-05 15:44:47 +0100

    Removed commissioning UI elements until back end implemented.

--- src/apps/tkinter/app.py
+++ src/apps/tkinter/app.py
@@ -10,17 +10,14 @@ def main():
     frame = ttk.Frame(root)
     frame.grid()
 
-
-    commission_button = ttk.Button(frame, text='Commission')
     toggle_button = ttk.Button(frame, text='Toggle')
     read_button = ttk.Button(frame, text='Read')
 
-    commission_button.grid(column=0, row=0, sticky='nsew')
-    toggle_button.grid(column=1, row=0, sticky='nsew')
-    read_button.grid(column=2, row=0, sticky='nsew')
+    toggle_button.grid(column=0, row=0, sticky='nsew')
+    read_button.grid(column=1, row=0, sticky='nsew')
 
     text = tk.Text(frame, wrap=tk.WORD)
-    text.grid(column=0, row=2, columnspan=3, sticky='nsew')
+    text.grid(column=0, row=2, columnspan=2, sticky='nsew')
 
 
     root.eval('tk::PlaceWindow . center')
