241ada48ef14f1c5526d38eddc46603a830cb583
Richard Wells <r.wells@cascoda.com>
2022-08-16 14:53:49 +0100

    Restructured cli app and added connection command.
    Added connection as a command instead of by default - removing hard coded values.

--- src/apps/cli/app.py
+++ src/apps/cli/app.py
@@ -2,27 +2,46 @@ from ...controller import Controller
 
 # Example will connect to a light, then toggle it on and off reading its value
 def main():
-    controller = Controller(1, "2a00:23a8:89b:da01:5bac:df45:7887:b359", 20202021, 1234)
-    
+    controller = False
+
     while True:
         choice = input(">")
+        args = choice.split(" ")
 
-        if choice == "commission":
-            controller.commission_device()
-        elif choice == "toggle":
-            print(controller.toggle_light())
-        elif choice == "read":
-            print(controller.read_light())
-        elif choice == "address":
-            print(controller.devCtrl.GetAddressAndPort(1234))
-        elif choice == "scan":
-            controller.scan()
-        elif choice == "discovered":
-            controller.print_discovered()
-        elif choice == "exit" or choice == "quit" or choice == "q":
-            controller.shutdown()
+        validCommand = False
+
+        if args[0] == "connect":
+            if len(args) != 4:
+                print("Incorrect number of args")
+            else:
+                controller = Controller(1, args[1], int(args[2]), int(args[3]))
+            validCommand = True
+        elif args[0] == "exit" or args[0] == "quit" or args[0] == "q":
+            if controller:
+                controller.shutdown()
             exit()
-        else:
+        elif controller:
+            if args[0] == "commission":
+                controller.commission_device()
+                validCommand = True
+            elif args[0] == "toggle":
+                print(controller.toggle_light())
+                validCommand = True
+            elif args[0] == "read":
+                print(controller.read_light())
+                validCommand = True
+            elif args[0] == "address":
+                print(controller.devCtrl.GetAddressAndPort(1234))
+                validCommand = True
+            elif args[0] == "scan":
+                print("Scanning devices in background...")
+                controller.scan()
+                validCommand = True
+            elif args[0] == "discovered":
+                controller.print_discovered()
+                validCommand = True
+        
+        if not validCommand:
             print("Unsupported choice")
 
 if __name__ == "__main__":
